b3c5da0d5c10bee32ecc90a7c64b16438bb403b5
Marcus <101027671+mlihk@users.noreply.github.com>
2024-03-28 14:29:42 +0800

    Create drop_tables.py
    1st version concept only

---
+++ Chronicles_of_Valor_1.0/drop_tables.py
@@ -0,0 +1,9 @@
+wild_boar_drop_table = ["Wooden Sword":0.2,
+                        "Wooden Axe":0.2,
+                       ]
+
+#loops through each item in the drop table dictionary,
+#and runs a rng role according to the chance, if successfully,
+#drop the item
+#need to write a drop item on the floor system for players to pick up
+#we would allow them to loot like wow? 
